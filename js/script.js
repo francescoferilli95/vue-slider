70b3413c7681d11832aaedcc3461202419f25362
francescoferilli95 <francesco.ferilli1995@libero.it>
2021-04-21 15:29:22 +0200

    adding vuejs app logic

--- js/script.js
+++ js/script.js
@@ -7,6 +7,44 @@
 const app = new Vue({
     el: '#app',
     data: {
-        
+        images: [
+            './img/image1.jpg',
+            './img/image2.jpg',
+            './img/image3.jpg',
+            './img/image4.jpg',
+        ],
+        indexImage: 0,
+        textImage: 'Landscape Image',
+        intervalID: 0,
+    },
+    created() {
+        this.startLoop();
+    },
+    methods: {
+        prevImage() {
+            this.indexImage--;
+
+            if(this.indexImage < 0) {
+                this.indexImage = this.images.length - 1;
+            }
+        },
+        nextImage() {
+            this.indexImage++;
+
+            if(this.indexImage > (this.images.length - 1)) {
+                this.indexImage = 0;
+            }
+        },
+        setImage(index) {
+            this.indexImage = index;
+        },
+        startLoop() {
+            this.intervalID = setInterval(() => {
+                this.nextImage();
+            }, 3000);
+        },
+        stopLoop() {
+            clearInterval(this.intervalID);
+        },
     },
 });
